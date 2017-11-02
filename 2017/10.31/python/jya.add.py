586cec49ceadd4cc19db4c34811c01576f545fa8
jya9055 <jya9055@gmail.com>
2017-11-02 16:56:40 +0900

    ???

--- 2017/10.31/python/jya.add.py
+++ 2017/10.31/python/jya.add.py
@@ -1,11 +1,13 @@
-def add(a, b):
-  return a+b
+<<<<<<< HEAD
+def Add(a, b, c, d, e):
+	return ((a + b + c)*(d - e))
+
+j = Add(3, 5, 7, 3, 3)
+print(j)
+=======
+def add(a, b, c):
+  return a + b + c
   
-  add(3, 5)
-  add(3, 5):
-  
-  
-  
-def add(a, b)=a+b:
-  add(4, 5);
-  
+j = add(3, 4, 5)
+print(j)
+>>>>>>> dfc0d2fc25bfa1b190acc4e3145a1aa24efb7653
