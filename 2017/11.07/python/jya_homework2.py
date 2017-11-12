3db983ee0392a7c8491ae5613ffa5d1264958d60
jya9055 <33053527+jya9055@users.noreply.github.com>
2017-11-13 07:52:03 +0900

    homework3

---
+++ 2017/11.07/python/jya_homework2.py
@@ -0,0 +1,14 @@
+    ```python
+
+    i = -1
+    sum = 0
+
+    one_to_ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    while i < 10:
+	    i += 1
+	    sum += one_to_ten[i]
+
+    print(sum)
+
+    ```
