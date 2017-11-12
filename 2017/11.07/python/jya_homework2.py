e7e1ecbf06b2d9abe2f6e7f78e4bd9010c2558a8
jya9055 <33053527+jya9055@users.noreply.github.com>
2017-11-13 08:00:53 +0900

    redo!!

--- 2017/11.07/python/jya_homework2.py
+++ 2017/11.07/python/jya_homework2.py
@@ -1,14 +1,14 @@
-    ```python
+```python
 
-    i = -1
-    sum = 0
+i = -1
+sum = 0
 
-    one_to_ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+one_to_ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    while i < 10:
-	    i += 1
-	    sum += one_to_ten[i]
+while i < 10:
+	i += 1
+	sum += one_to_ten[i]
 
-    print(sum)
+print(sum)
 
-    ```
+```
