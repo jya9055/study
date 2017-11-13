db57c4218b1327d18219a8a655f6650e6758dbbb
jya9055 <33053527+jya9055@users.noreply.github.com>
2017-11-14 08:40:07 +0900

    again

--- 2017/11.07/python/jya_homework2.py
+++ 2017/11.07/python/jya_homework2.py
@@ -12,18 +12,3 @@ result = sum10(one_to_ten)
 print(result)
 
 ```
-
-**Make function!**
-`like:`
-```python
-def sum(number):
-    # .. TODO
-    return 0
-```
-
-`I want to reuse this function, like:`
-```python
-one_to_ten = sum(range(1,11))
-one_to_five = sum(range(1,6))
-five_to_ten = sum(range(5,11))
-```
