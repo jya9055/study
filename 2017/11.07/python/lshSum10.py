116f47275a6f3fba63f8ba12bcac87132728e60f
김민경 <minkyeong@gimmingyeong-ui-MacBook-Air.local>
2017-11-14 18:11:07 +0900

    commit sum10
    yes~

--- 2017/11.07/python/lshSum10.py
+++ 2017/11.07/python/lshSum10.py
@@ -1,8 +1,10 @@
 def sum10(number):
-        # TODO
-        return 0
+        sum 0 
+        for i in one_to_ten:
+        sum += in
+        return sum
 
 one_to_ten = range(1,11)
 result = sum10(one_to_ten)
 
-print(result) # It will be: 55
+print(result) 
