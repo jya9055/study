8f200281174fa046303b73d54337e3d0d99fc5ac
Sanghak,Lee <code.ryan.lee@gmail.com>
2017-11-29 16:14:39 +0900

    lsh-edit-excel.py Done!

--- 2017/11.28/python/lsh-edit.excel.py
+++ 2017/11.28/python/lsh-edit.excel.py
@@ -1,1 +1,20 @@
 # Let do it!
+from openpyxl import load_workbook
+
+wb = load_workbook('../datas/daily_gabia_20171127.xlsx') # 엑셀 열기
+ws = wb.active # 현재 워크시크 가져오기
+
+
+E = ws['E4':'E10'] 
+
+column_idx = 5
+
+for t in E: # ( (), () )
+    row_idx = t[0].row # 현재 행 index
+    print('row_idx: {0}'.format(row_idx))
+    for c in t: # t는 튜플 ( )
+        print('now_value: {0}'.format(c.value))
+        ws.cell(row=row_idx, column=column_idx).value = c.value + 2
+
+wb.save('../datas/lsh-edit.xlsx') # 엑셀 파일 저장
+wb.close()
