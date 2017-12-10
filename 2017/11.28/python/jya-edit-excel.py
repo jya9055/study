c8ff08eefe4b986f2466611bb379a92048af91d8
jya9055 <jya9055@gmail.com>
2017-12-10 20:02:11 +0900

    homework_1210

--- 2017/11.28/python/jya-edit-excel.py
+++ 2017/11.28/python/jya-edit-excel.py
@@ -5,6 +5,51 @@ from openpyxl import load_workbook
 wb = load_workbook('../datas/daily_gabia_20171127.xlsx')
 ws = wb.active
 
-ws['E4:E10'.value] =+ 2
+def edit(a):
+    return  a + 2
 
-wb.save('../datas/jya-edit.xlsx')
+
+c1 = ws['E4']
+d = c1.value
+k = edit(d)
+ws['E4'] = k
+
+c2 = ws['E5']
+d = c2.value
+k = edit(d)
+ws['E5'] = k
+
+c3 = ws['E6']
+d = c3.value
+k = edit(d)
+ws['E6'] = k
+
+c4 = ws['E7']
+d = c4.value
+k = edit(d)
+ws['E7'] = k
+
+c5 = ws['E8']
+d = c5.value
+k = edit(d)
+ws['E8'] = k
+
+c6 = ws['E9']
+d = c6.value
+k = edit(d)
+ws['E9'] = k
+
+c7 = ws['E10']
+d = c7.value
+k = edit(d)
+ws['E10'] = k
+
+##A = ws['E4':'E10']
+#for t in A:
+#    for c in t:
+#        d = c.value
+#        t = edit(d)
+#        for i in range (0, 7):
+#            A[i] = t
+
+wb.save('../datas/daily_gabia_20171127_edit.xlsx')
