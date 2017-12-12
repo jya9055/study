4f170a52e57485d439cdd6bd8d503c525c9ce886
Sanghak,Lee <code.ryan.lee@gmail.com>
2017-12-12 09:32:17 +0900

    - 이중 for -> 단일 for - 열 번호 가져오기 - 함수화

--- 2017/11.28/python/lsh-edit.excel.py
+++ 2017/11.28/python/lsh-edit.excel.py
@@ -1,20 +1,20 @@
 # Let do it!
 from openpyxl import load_workbook
+from openpyxl.utils import column_index_from_string
 
 wb = load_workbook('../datas/daily_gabia_20171127.xlsx') # 엑셀 열기
 ws = wb.active # 현재 워크시크 가져오기
 
+def edit(cell, append):
+    for t in cell: # ( (), () )
+        row_idx = t[0].row # 현재 행 index
+        column_idx = column_index_from_string(t[0].column)
+        row_value = t[0].value
+        ws.cell(row=row_idx, column=column_idx).value = row_value + append
 
 E = ws['E4':'E10'] 
 
-column_idx = 5
-
-for t in E: # ( (), () )
-    row_idx = t[0].row # 현재 행 index
-    print('row_idx: {0}'.format(row_idx))
-    for c in t: # t는 튜플 ( )
-        print('now_value: {0}'.format(c.value))
-        ws.cell(row=row_idx, column=column_idx).value = c.value + 2
+edit(E, 3)
 
 wb.save('../datas/lsh-edit.xlsx') # 엑셀 파일 저장
 wb.close()
