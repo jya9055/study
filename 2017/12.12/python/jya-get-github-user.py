edd329685ef0ccd806c9529b6849341403f22996
jya9055 <jya9055@gmail.com>
2018-01-29 20:45:33 +0900

    good game

--- 2017/12.12/python/jya-get-github-user.py
+++ 2017/12.12/python/jya-get-github-user.py
@@ -1,16 +1,16 @@
 import requests
 
-#params = {'name':'', 'blog':''}
 r = requests.get('https://api.github.com/users/jya9055')
 j = r.json()
 
-def getUser(id):
-    return j['name'] + "/" + j['blog']
-    if j['name'] == '':
-        return j['login']
-    if j['blog'] == '':
-        return j['html_url']
+name = j['name']
+blog = j['blog']
+html_url = j['html_url']
 
-d = getUser(id)
-print(d)
+if not blog:
+    print name + '/ ' + html_url
+
+elif blog:
+    print name + '/ ' + blog 
+
 
--- 2018/01.23/API/jya.api.py
+++ 2018/01.23/API/jya.api.py
@@ -1,22 +1,23 @@
 import requests
 
-
-
-def getprice(coin): 
+def getPrice(A): 
     # coin 파라미터  사용!
     # https://api.coinnest.co.kr/api/pub/ticker?coin=eth
     # https://api.coinnest.co.kr/api/pub/ticker?coin={변수}
     # .format()
-    r = requests.get('https://api.coinnest.co.kr/api/pub/ticker')
-    j = r.json()
-	coin = j["coin"]
-	last = j["last"]
-	co_list = [btc, bch, btg, bcd, eth, etc, ada, qtum, xlm, neo, gas, rpx, hsr, knc, tsl, tron, omg, wtc, mco, storm, gto, pxs, ink, hlc, ent, 	qbt, spc, put 
-		if coin not in co_list
-		    return "false"
-		else:
-			return j["last"]
 
-a = getprice(qtum)
+	#. 문자형 .format() 인자값
+	#. () 안에 들어오는 코인은 정해져 있고, 정해진 코인이 아닐 경우 false 리턴
+	#. 정해진 코인을 입력한 경우, 해당 코인의 마지막 가격을 리턴
+	
+	r = requests.get('https://api.coinnest.co.kr/api/pub/ticker?coin={coin}'.format(A))
+	j = r.json()
+	A = j['coin']
+	if A not in coin:
+		return "false"
+	else:
+		return j["last"]
+
+a = getPrice(qtum)
 print(a)
 
