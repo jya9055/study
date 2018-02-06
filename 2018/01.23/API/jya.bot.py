4995094093da51e7488dfccff72b98c5d981fdc1
jya9055 <jya9055@gmail.com>
2018-02-06 15:30:47 +0900

    dddddd

--- 2018/01.23/API/jya.bot.py
+++ 2018/01.23/API/jya.bot.py
@@ -1,5 +1,5 @@
 # 함수 가져옵니다
-import requests
+import requests, threading
 def getPrice(coin):
 	A = ['btc','bch','btg','bcd','ubtc','eth','etc','ada','qtum','xlm','neo','gas','rpx','hsr','knc','tsl','tron','omg','wtc','mco','storm','gto','pxs','chat','ink','hlc','ent','qbt','spc','put']
 	if coin not in A:
