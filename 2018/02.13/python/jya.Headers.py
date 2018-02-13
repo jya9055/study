b0358e5e7fbe321b387466a0081e82aca1b63bd3
jya9055 <jya9055@gmail.com>
2018-02-13 17:20:49 +0900

    dd

--- 2018/02.13/python/jya.Headers.py
+++ 2018/02.13/python/jya.Headers.py
@@ -1,15 +1,19 @@
 import requests
 
-def makeHeadersAuth():
-    
-    
 
-    return {'Authorization': 'Basic {key}'}
-
-    def getToken():
+def getToken():
     r = requests.post('https://gapi.gabia.com/oauth/token', data={'client_id': 'www_front', 'client_secret': 'eoqhfma', 'grant_type': 'client_credentials'})
     j = r.json()
     token = j['access_token']
     return token
-    
 
+print (getToken())
+p = getToken()
+
+def makeHeadersAuth(token):   
+    token = getToken()
+    auth = btoa('www_front:{0}'.format(token))
+    print(auth)
+    return {'Authorization': 'Basic {0}'.format(auth)}
+
+def mak
