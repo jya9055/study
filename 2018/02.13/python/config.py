ee4315bea088d4d556321070e9aa71f38248e3b2
jya9055 <jya9055@gmail.com>
2018-03-13 15:18:36 +0900

    changeformat&deleteunnecessaryfile

--- 2018/02.13/python/config.py
+++ 2018/02.13/python/config.py
@@ -7,7 +7,7 @@ def get_config(config):
     client_id = config.get('gAPI', 'client_id')
     client_secret = config.get('gAPI', 'client_secret')
     grant_type = config.get('gAPI', 'grant_type')
-    return 'client_id={0}&client_secret={1}&grant_type={2}'.format(client_id, client_secret, grant_type)
+    return "'client_id': '{0}', 'client_secret': '{1}', 'grant_type': '{2}'".format(client_id, client_secret, grant_type)
 
-# d = get_config(config)
-# print(d)
+d = get_config(config)
+print(d)
