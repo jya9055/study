09132958e94f3502314c2b1e50006eb79883c75d
jya9055 <jya9055@gmail.com>
2018-03-13 15:10:27 +0900

    test_gitignore

---
+++ 2018/02.13/python/config.py
@@ -0,0 +1,13 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('../../config.ini')
+
+def get_config(config):
+    client_id = config.get('gAPI', 'client_id')
+    client_secret = config.get('gAPI', 'client_secret')
+    grant_type = config.get('gAPI', 'grant_type')
+    return 'client_id={0}&client_secret={1}&grant_type={2}'.format(client_id, client_secret, grant_type)
+
+d = get_config(config)
+print(d)
