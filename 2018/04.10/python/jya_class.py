8473b77ce2019ee058b83f451e8dee4a1f0bde52
jya9055 <jya9055@gmail.com>
2018-04-19 08:52:43 +0900

    update jya_class.py

--- 2018/04.10/python/jya_class.py
+++ 2018/04.10/python/jya_class.py
@@ -7,40 +7,40 @@ type = config.GAPI_CONFIG['grant_type']
 
 class GapiClass:
     def __init__(self, host='https://gapi.gabia.com'):
-        self.host = host
-        self.headers = self.encoded_token()
+        self.__host = host
+        self.__headers = self.__encoded_token()
 
-    def Requests_get(self, url):
-        r = requests.get('{0}{1}'.format(self.host, url), headers = self.headers)
+    def __Requests_get(self, url):
+        r = requests.get('{0}{1}'.format(self.__host, url), headers = self.__headers)
         j = r.json()
         return j
 
-    def Requests_post(self, url, data):
-        r = requests.post('{0}{1}'.format(self.host, url), data = data)
+    def __Requests_post(self, url, data):
+        r = requests.post('{0}{1}'.format(self.__host, url), data = data)
         j = r.json()
         return j
 
-    def getToken(self):
-        j = self.Requests_post('/oauth/token', {'client_id': id, 'client_secret': secret, 'grant_type': type})
+    def __getToken(self):
+        j = self.__Requests_post('/oauth/token', {'client_id': id, 'client_secret': secret, 'grant_type': type})
         token_1 = j['access_token']
         token_2 = 'www_front:{0}'.format(token_1) 
         return token_2
 
-    def makeHeadersAuth(self, token):
+    def __makeHeadersAuth(self, token):
         encoded_text = token.encode()
         k = base64.b64encode(encoded_text)
         l = k.decode()
         return {'Authorization': 'Basic {0}'.format(l)}
 
-    def encoded_token(self):
-        return self.makeHeadersAuth(self.getToken())
+    def __encoded_token(self):
+        return self.__makeHeadersAuth(self.__getToken())
 
     def getMember(self, id):
-        j = self.Requests_get('/members?user_id={0}'.format(id))
+        j = self.__Requests_get('/members?user_id={0}'.format(id))
         hanname = j['client_info']['hanadmin']
         return hanname
 
 # api1 = GapiClass()
-# a = api1.getToken()
+# a = api1.getMember('planning_d')
 # print(a)
 
