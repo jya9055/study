c276a88dce8d34fba68e20f88770b93801eb610a
jya9055 <jya9055@gmail.com>
2018-08-10 13:51:19 +0900

    completegit add result.html

--- 2018/08.05/app.py
+++ 2018/08.05/app.py
@@ -1,25 +1,58 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from jya_gAPIclass import GapiClass
+import pystache
+import codecs
 
 app = Flask(__name__)
 
 @app.route('/mail')
 def mail():
-	return render_template('mailcontents.html')
+	return render_template('form.html')
 
-# 1. /mail 페이지에서 정보(1, 2)를 입력 받아, [발송] 버튼을 누르면 /send로 이동
+
+@app.route('/result', methods=['POST'])
+def result():
+	if request.method == 'POST':
+		# /mail에서 입력 받은 변수 가져오기
+		k = request.form
+		hanname = k['hanname']
+		regist_date = k['regist_date']
+		service_name = k['service_name']
+		domain = k['domain']
+		expiration_date = k['expiration_date']
+		extension_expense = k['extension_expense']
+		extension_period = k['extension_period']
+		extendable_limit = k['extendable_limit']
+		total_count = k['total_count']
+		mail = k['이메일']
+		
+		# 메일 콘텐츠에 변수 넣어주기
+		f = codecs.open('/Users/조양아/Documents/GitHub/study/2018/08.05/templates/mail_contents.html', 'r', encoding='utf-8')
+		text = f.read()
+		data = {
+			'hanname':hanname,
+			'regist_date':regist_date,
+			'service_name':service_name,
+			'domain':domain,
+			'expiration_date':expiration_date,
+			'extension_expense':extension_expense,
+			'extension_period':extension_period,
+			'extendable_limit':extendable_limit,
+			'total_count':total_count
+		}
+		mail_text = pystache.render(text, data)
+		
+		# 메일 발송하기
+		subject = '[가비아] 서비스 이용 기간을 연장해 주세요'
+		instance = GapiClass()
+		instance.sendMail(subject, mail_text, mail, hanname)
+		return render_template('result.html')
+
 
 @app.route('/send')
 def send():
-    instance = GapiClass()
-	instance.sendMail(mail_title , mail_text, email, name)
+	return '발송 완료'
 
-# 2. 입력된 정보(1)를 바탕으로 pystache를 이용하여 메일 콘텐츠를 만들어주고
-# 3. 입력된 정보(2)를 함수(sendMail)에 들어가는 변수로 넣어줌
-# 4. 앞의 과정이 완료되면 실제 메일 발송하고 완료 메시지를 띄움 ('발송되었습니다')
-
-# 입력된 정보를 활용하는 법을 모르겠어요!!
 
 if __name__ == '__main__':
-	app.run()
+    app.run(debug=True)
